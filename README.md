2e7728234b708aaf7dd148ec010a16692e9af140
msnape90 <msnape90@gmail.com>
2026-03-26 15:02:36 -0400

    dev-env inital script and config commit

---
+++ README.md
@@ -0,0 +1,3 @@
+TODO: create a new repo for this project
+  TODO: make sure there are no secret keys in the code
+TODO:
---
+++ dotfiles/.config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+return {
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
