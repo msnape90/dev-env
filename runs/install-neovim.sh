216c5fa5c149b455f0d0e8f8028656dc4bcec87c
msnape90 <msnape90@gmail.com>
2026-04-01 15:18:45 -0400

    split node, wezterm, cbuild

--- runs/install-neovim.sh
+++ runs/install-neovim.sh
@@ -2,7 +2,6 @@
 
 # TODO:
 # test tectonic
-# install wezterm
 #
 echo "starting Neovim Install"
 
@@ -39,27 +38,27 @@ sudo apt update -y && sudo apt install ripgrep fd-find xclip python3-pip \
 
 #### NODE
 
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-
-# set up nvm to run in current terminal
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-# configure nvm
-nvm install --lts
-nvm use --lts
-nvm alias default 'lts/*'
-source ~/.bashrc
-
-# add npm globals bin to path
-NPMBIN="$(npm config get prefix)/bin"
-LINETOADD='export PATH="$PATH:'"$NPMBIN"'"'
-
-grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
-
-source "$HOME/.bashrc"
+# # install nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+#
+# # set up nvm to run in current terminal
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+#
+# # configure nvm
+# nvm install --lts
+# nvm use --lts
+# nvm alias default 'lts/*'
+# source ~/.bashrc
+#
+# # add npm globals bin to path
+# NPMBIN="$(npm config get prefix)/bin"
+# LINETOADD='export PATH="$PATH:'"$NPMBIN"'"'
+#
+# grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
+#
+# source "$HOME/.bashrc"
 
 # install npm packages
 npm install -g neovim
@@ -68,19 +67,19 @@ npm install -g @mermaid-js/mermaid-cli
 
 ### RUST
 
-install rust and cargo
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-
-. "$HOME/.cargo/env"
-
-# add cargo to path
-CARGO="$HOME/.cargo/bin"
-LINETOADD='export PATH="$PATH:'"$CARGO"'"'
-
-grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
-
-source "$HOME/.bashrc"
-
+# install rust and cargo
+# curl https://sh.rustup.rs -sSf | sh -s -- -y
+#
+# . "$HOME/.cargo/env"
+#
+# # add cargo to path
+# CARGO="$HOME/.cargo/bin"
+# LINETOADD='export PATH="$PATH:'"$CARGO"'"'
+#
+# # grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
+#
+# source "$HOME/.bashrc"
+#
 cargo install ast-grep --locked
 
 # install tectonic dependencies
@@ -97,13 +96,13 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 source "$HOME/.bashrc"
 
-##### NERD FONT
-# install nerd font
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip &&
-  cd ~/.local/share/fonts &&
-  unzip JetBrainsMono.zip &&
-  rm JetBrainsMono.zip &&
-  fc-cache -fv
+###### NERD FONT
+## install nerd font
+#wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip &&
+#  cd ~/.local/share/fonts &&
+#  unzip JetBrainsMono.zip &&
+#  rm JetBrainsMono.zip &&
+#  fc-cache -fv
 
 # set gnome terminal to nerd font
 PROFILE_ID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
@@ -138,9 +137,9 @@ uv venv ~/.local/share/nvim/python3 &&
   mkdir -p /home/$USER/.config/nvim &&
   echo "vim.g.python3_host_prog = '/home/$USER/.local/share/nvim/python3/bin/python'" >>/home/$USER/.config/nvim/init.lua
 
-# install wezterm
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg &&
-  echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list &&
-  sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
-
-sudo apt update && sudo apt install wezterm-nightly -y
+# # install wezterm
+# curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg &&
+#   echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list &&
+#   sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+#
+# sudo apt update && sudo apt install wezterm-nightly -y
